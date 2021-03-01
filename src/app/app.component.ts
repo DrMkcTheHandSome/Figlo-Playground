fab283cd54f7a3670321828cc6e982e3624e4aa6
Marc Kenneth Lomio <drmkcthehandsome@gmail.com>
2021-03-01 08:49:37 +0800

    setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,106 @@
+import { Subscription } from 'rxjs';
+// Angular
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+// Layout
+import { LayoutConfigService, SplashScreenService, TranslationService } from './core/_base/layout';
+// language list
+import { locale as enLang } from './core/_config/i18n/en';
+import { locale as chLang } from './core/_config/i18n/ch';
+import { locale as esLang } from './core/_config/i18n/es';
+import { locale as jpLang } from './core/_config/i18n/jp';
+import { locale as deLang } from './core/_config/i18n/de';
+import { locale as frLang } from './core/_config/i18n/fr';
+
+export interface PeriodicElement {
+	name: string;
+	position: number;
+	weight: number;
+	symbol: string;
+  }
+  
+  const ELEMENT_DATA: PeriodicElement[] = [
+	{position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+	{position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+	{position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+	{position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+	{position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+	{position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+	{position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+	{position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+	{position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+	{position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+
+@Component({
+	// tslint:disable-next-line:component-selector
+	selector: 'body[kt-root]',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent implements OnInit, OnDestroy {
+	// Public properties
+	title = 'Metronic';
+	loader: boolean;
+	private unsubscribe: Subscription[] = []; // Read more: => https://brianflove.com/2016/12/11/anguar-2-unsubscribe-observables/
+
+	displayedColumns: string[] = ['name', 'weight', 'position', 'symbol'];
+  	dataSource = ELEMENT_DATA;
+
+	/**
+	 * Component constructor
+	 *
+	 * @param translationService: TranslationService
+	 * @param router: Router
+	 * @param layoutConfigService: LayoutCongifService
+	 * @param splashScreenService: SplashScreenService
+	 */
+	constructor(private translationService: TranslationService,
+				         private router: Router,
+				         private layoutConfigService: LayoutConfigService,
+				         private splashScreenService: SplashScreenService) {
+
+		// register translations
+		this.translationService.loadTranslations(enLang, chLang, esLang, jpLang, deLang, frLang);
+	}
+
+	/**
+	 * @ Lifecycle sequences => https://angular.io/guide/lifecycle-hooks
+	 */
+
+	/**
+	 * On init
+	 */
+	ngOnInit(): void {
+		// enable/disable loader
+		this.loader = this.layoutConfigService.getConfig('loader.enabled');
+
+		const routerSubscription = this.router.events.subscribe(event => {
+			if (event instanceof NavigationEnd) {
+				// hide splash screen
+				this.splashScreenService.hide();
+
+				// scroll to top on every route change
+				window.scrollTo(0, 0);
+
+				// to display back the body content
+				setTimeout(() => {
+					document.body.classList.add('kt-page--loaded');
+				}, 500);
+			}
+		});
+		this.unsubscribe.push(routerSubscription);
+	}
+
+	/**
+	 * On Destroy
+	 */
+	ngOnDestroy() {
+		this.unsubscribe.forEach(sb => sb.unsubscribe());
+	}
+
+	openMe(){
+		alert("test")
+	}
+}
